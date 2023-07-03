6a827ed150972ffac936ec72617b21b1834f6fc4
guifeel <guifeel@qq.com>
2023-07-03 18:48:52 +0800

    提取通用hook

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,11 +1,11 @@
-import { SimpleGrid, Skeleton, Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
-import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
+import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data: games, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>

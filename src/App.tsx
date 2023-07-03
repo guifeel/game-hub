6b9bef63847aa38a0509eefaa33d2681a5f8f7c0
guifeel <guifeel@qq.com>
2023-07-03 12:38:23 +0800

    创建布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "  main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={'nav'} bg={'orange.300'}>
+          nav
+        </GridItem>
+        <Show above='lg'>
+          <GridItem area={'aside'} bg={'pink.300'}>
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area={'main'} bg={'green.300'}>
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }

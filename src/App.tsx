332a28936cedfd8d58a08bf26f35732d52b28624
guifeel <guifeel@qq.com>
2023-07-03 22:58:19 +0800

    渲染游戏平台

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GameGrid from './Components/GameGrid';
 import GenreList from './Components/GenreList';
 import { useState } from 'react';
 import { Genre } from './hooks/useGenres';
+import PlatformSelector from './Components/PlatformSelector';
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -29,6 +30,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area={'main'}>
+          <PlatformSelector />
           <GameGrid selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>

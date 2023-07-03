c8dab8593d4d78e9b5363aec72875d265d2aeb1f
guifeel <guifeel@qq.com>
2023-07-03 19:32:02 +0800

    渲染spinner

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,9 +1,11 @@
-import { HStack, Image, List, Text } from '@chakra-ui/react';
+import { HStack, Image, List, Spinner, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/img-url';
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((genre) => (

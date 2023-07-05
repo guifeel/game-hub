015097cc2de474002e66cf637999504b6b265098
guifeel <guifeel@qq.com>
2023-07-05 21:33:53 +0800

    缓存platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import apiClient from '../services/api-client';
 import { FetchResponse } from './useData';
 import { Platform } from './useGames';
+import platforms from '../data/platforms';
 
 // interface Platform {
 //   id: number;
@@ -18,6 +19,8 @@ const usePlatforms = () =>
       apiClient
         .get<FetchResponse<Platform>>('/platforms/lists/parents')
         .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;

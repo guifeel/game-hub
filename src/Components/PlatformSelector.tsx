a966e9d89e93bd1ccac0e1b102a8772b77782b98
guifeel <guifeel@qq.com>
2023-07-05 21:30:39 +0800

    获取平台

--- src/Components/PlatformSelector.tsx
+++ src/Components/PlatformSelector.tsx
@@ -17,7 +17,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || '平台'}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             key={platform.id}
             onClick={() => onSelectPlatform(platform)}
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import platforms from '../data/platforms';
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import { FetchResponse } from './useData';
+import { Platform } from './useGames';
 
 // interface Platform {
 //   id: number;
@@ -7,6 +10,14 @@ import platforms from '../data/platforms';
 // }
 
 // const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
-const usePlatforms = () => ({ data: platforms, error: null });
+// const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ['platforms'],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>('/platforms/lists/parents')
+        .then((res) => res.data),
+  });
 
 export default usePlatforms;

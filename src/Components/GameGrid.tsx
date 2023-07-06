d87e71ebf431ebde8868d0598f14eef863499530
guifeel <guifeel@qq.com>
2023-07-06 10:12:51 +0800

    删除未使用引入

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -12,14 +12,8 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    hasNextPage,
-    isFetchingNextPage,
-    fetchNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, hasNextPage, fetchNextPage } =
+    useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   if (error) return <Text>{error.message}</Text>;
   const fetchGamesCount =

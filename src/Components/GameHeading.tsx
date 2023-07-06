1adc4237da2b2fcb3d6a75a2e7efd22dc849af18
guifeel <guifeel@qq.com>
2023-07-06 11:57:58 +0800

    自定义genre和platform的hook

--- src/Components/GameHeading.tsx
+++ src/Components/GameHeading.tsx
@@ -1,18 +1,17 @@
 import { Heading } from '@chakra-ui/react';
 import { GameQuery } from '../App';
-import useGenres from '../hooks/useGenres';
+import useGenre from '../hooks/useGenre';
 import usePlatforms from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
+  const genre = useGenre(gameQuery.genreId);
 
-  const { data: plaforms } = usePlatforms();
-  const platform = plaforms?.results.find((p) => p.id === gameQuery.platformId);
+  const platform = usePlatform(gameQuery.platformId);
 
   const heading = `${platform?.name || ''} ${genre?.name || ''} 游戏`;
   return (
--- src/Components/PlatformSelector.tsx
+++ src/Components/PlatformSelector.tsx
@@ -1,6 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 import usePlatforms, { Platform } from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
   onSelectPlatform: (plaform: Platform) => void;
@@ -8,8 +9,7 @@ interface Props {
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
-  const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find((p) => p.id === selectedPlatformId);
+  const platform = usePlatform(selectedPlatformId);
   const { data, error } = usePlatforms();
   if (error) return null;
   return (

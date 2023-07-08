514153049f3188ee9e2f285c1a7b781f1895ae51
guifeel <guifeel@qq.com>
2023-07-08 16:54:45 +0800

    展示一个游戏

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,5 +1,18 @@
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/useGame';
+import { Box, Heading, Spinner, Text } from '@chakra-ui/react';
+
 const GameDetail = () => {
-  return <div>GameDetail</div>;
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGame(slug!);
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+  return (
+    <Box padding={5}>
+      <Heading py={5}>{game?.name}</Heading>
+      <Text>{game?.description_raw}</Text>
+    </Box>
+  );
 };
 
 export default GameDetail;
